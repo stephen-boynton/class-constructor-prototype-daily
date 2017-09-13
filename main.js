7f7a9f7d617e76fcac27ac7930d90a80620d8b53
Stephen Boynton <boyntonsc@gmail.com>
2017-09-13 16:37:05 -0400

    final

--- main.js
+++ main.js
@@ -7,10 +7,19 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog () {}
+function Dog(name, color, fed) {
+	this.name = name;
+	this.color = color || "brown";
+	this.hungry = fed || false;
+	this.status = "normal";
+}
 
 // Instances of Dog
 // Needed: sadie, moonshine, atticus
+
+const sadie = new Dog("sadie", "black");
+const moonshine = new Dog("moonshine", "brown", true);
+const atticus = new Dog("atticus");
 
 //     __
 //    / /_  __  ______ ___  ____ _____  _____
@@ -19,7 +28,21 @@ function Dog () {}
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human () {}
+function Human(name, cool) {
+	this.name = name;
+	this.cool = cool;
+}
+
+Human.prototype.pet = function(dog) {
+	dog.status = "happy";
+};
+
+Human.prototype.feed = function(dog) {
+	dog.hungry = false;
+};
 
 // Instances of Human
 // Needed: mason, julia
+
+const mason = new Human("mason", false);
+const julia = new Human("julie", true);
